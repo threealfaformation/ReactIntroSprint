c0359c3543b5a59c326276a78f7ea61ac5365fad
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2025-10-21 19:49:11 +0100

    postrequestsolution

--- client/src/Types.ts
+++ client/src/Types.ts
@@ -6,3 +6,15 @@ export interface Pokemon {
  _id : string
 
 }
+
+export interface Trainer {
+  _id: string;
+  name: string;
+  level : number
+}
+
+export interface Zone {
+  _id: string;
+  name: string;
+  description : string
+}
