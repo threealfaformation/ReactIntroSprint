8e109d236d0d64baa8e8518fc6d46510f1e68722
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2025-10-21 19:48:47 +0100

    postrequestsolution

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Pokedex from "./components/Pokedex";
 import Pokedetails from "./components/Pokedetails";
+import Newpokemon from "./components/newPokemon";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
@@ -9,6 +10,8 @@ const App: React.FC = () => {
 
         <Route path="/" element={<Pokedex />} />
         <Route path="/pokemondetails/:id" element={<Pokedetails />} />
+        <Route path="/addpokemon" element={<Newpokemon />} />
+
       </Routes>
     </BrowserRouter>
   );
